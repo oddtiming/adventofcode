f350c0cae64582930f21b4779861983737cfa450
Ismael Yahyaoui Racine <ismaelyahyaouiracine@Ismaels-MacBook-Pro.local>
2021-12-08 23:52:21 -0500

    solver works, gotta add up total

--- day08/seven_segments.c
+++ day08/seven_segments.c
@@ -2,63 +2,379 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <strings.h>
+#include <string.h>
 
-void	count_1478(char *line, int **digits_count)
+int	is_set(char const c, char const *set)
 {
-	int	i, word_len;
+	while (*set)
+	{
+		if (*set == c)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+//Needs to be corrected to avoid freeing errors;
+void	free_array(char **arr)
+{
+	int	i;
 
 	i = 0;
-	word_len = 0;
-	while (line[i])
+	while (arr && *arr && arr[i])
 	{
+		free(arr[i]);
+		i++;
+	}
+	if (arr && *arr)
+	{
+		free(arr[i]);
+		free(arr);
+	}
+}
+
+char	*get_next_word(const char *str, const char *sep)
+{
+	static int	i;
+	int			word_len;
+	char		*next_word;
+
+	word_len = 0;
+	while (str[i] && !is_set(str[i], sep))
+	{
+		i++;
+		word_len++;
+	}
+	next_word = malloc(word_len + 1);
+	i -= word_len;
+	word_len = 0;
+	while (str[i] && !is_set(str[i], sep))
+	{
+		next_word[word_len] = str[i];
+		i++;
+		word_len++;
+	}
+	next_word[word_len] = 0;
+	if (str[i])
+		i++;
+	else
 		i = 0;
-		while (line[++i] != '|') ;
-		while (!isalpha(line[++i])) ;
-		while (line[i])
+	return (next_word);
+}
+
+char	**parse_line(char *curr_line)
+{
+	int		i;
+	char	**input_digits;
+
+	i = 0;
+	input_digits = malloc(11 * sizeof(char *));
+	while (i < 10)
+	{
+		input_digits[i] = get_next_word(curr_line, " ");
+		i++;
+	}
+	input_digits[i] = NULL;
+	return (input_digits);
+}
+
+int	get_diff_charset(const char *s1, const char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int	diff = 0;
+	int found = 0;
+
+	while (s1[i])
+	{
+		j = 0;
+		while (s2[j])
 		{
-			if (isalpha(line[i]))
-				word_len++;
-			else
-			{
-				if (word_len == 2)
-					*(*digits_count + 1) += 1;
-				else if (word_len == 3)
-					*(*digits_count + 7) += 1;
-				else if (word_len == 4)
-					*(*digits_count + 4) += 1;
-				else if (word_len == 7)
-					*(*digits_count + 8) += 1;
-				word_len = 0;
-			}
-			i++;
+			if (s1[i] == s2[j])
+				found = 1;
+			j++;
+		}
+		if (!found)
+			diff++;
+		found = 0;
+		i++;
+	}
+	return (diff);
+}
+
+//no diff with both ==> 9
+//no diff with 1 ==> 0 (b/c 5 has only 1 diff with 4)
+void	decipher_6s(char **char_inputs, int **int_values, int *str_len6)
+{
+	int	i, diff3over0, diff3over1, diff3over2;
+
+	i = 0;
+	//get_diff_charset will return 0 if s1 == 3 and s2 == 9
+	diff3over0 = get_diff_charset(char_inputs[*(*int_values + 3)], char_inputs[str_len6[0]]);
+	diff3over1 = get_diff_charset(char_inputs[*(*int_values + 3)], char_inputs[str_len6[1]]);
+	diff3over2 = get_diff_charset(char_inputs[*(*int_values + 3)], char_inputs[str_len6[2]]);
+	if (!diff3over0)
+	{
+		*(*int_values + 9) = str_len6[0];
+		//1 diff with 1 ==> 6
+		if (get_diff_charset(char_inputs[*(*int_values + 1)], char_inputs[str_len6[1]]) == 0)
+		{
+			*(*int_values + 0) = str_len6[1];
+			*(*int_values + 6) = str_len6[2];
+			return ;
+		}
+		else
+		{
+			*(*int_values + 0) = str_len6[2];
+			*(*int_values + 6) = str_len6[1];
+			return ;
+		}
+	}
+	else if (!diff3over1)
+	{
+		*(*int_values + 9) = str_len6[1];
+		//1 diff with 1 ==> 6
+		if (get_diff_charset(char_inputs[*(*int_values + 1)], char_inputs[str_len6[0]]) == 0)
+		{
+			*(*int_values + 0) = str_len6[0];
+			*(*int_values + 6) = str_len6[2];
+			return ;
+		}
+		else
+		{
+			*(*int_values + 0) = str_len6[2];
+			*(*int_values + 6) = str_len6[0];
+			return ;
+		}
+	}
+	else if (!diff3over2)
+	{
+		*(*int_values + 9) = str_len6[2];
+		//1 diff with 1 ==> 6
+		if (get_diff_charset(char_inputs[*(*int_values + 1)], char_inputs[str_len6[0]]) == 0)
+		{
+			*(*int_values + 0) = str_len6[0];
+			*(*int_values + 6) = str_len6[1];
+			return ;
+		}
+		else
+		{
+			*(*int_values + 0) = str_len6[1];
+			*(*int_values + 6) = str_len6[0];
+			return ;
 		}
 	}
 }
 
+void	decipher_5s(char **char_inputs, int **int_values, int *str_len5)
+{
+	int	i, diff01, diff12, diff02;
+
+	i = 0;
+	diff01 = get_diff_charset(char_inputs[str_len5[0]], char_inputs[str_len5[1]]);
+	diff12 = get_diff_charset(char_inputs[str_len5[1]], char_inputs[str_len5[2]]);
+	diff02 = get_diff_charset(char_inputs[str_len5[0]], char_inputs[str_len5[2]]);
+	if (diff01 == 1)
+	{
+		if (diff02 == 1)
+		{
+			*(*int_values + 3) = str_len5[0];
+			if (get_diff_charset(char_inputs[str_len5[2]], char_inputs[*(*int_values + 4)]) == 2)
+			{
+				*(*int_values + 5) = str_len5[2];
+				*(*int_values + 2) = str_len5[1];
+				return ;
+			}
+			else
+			{
+				*(*int_values + 5) = str_len5[1];
+				*(*int_values + 2) = str_len5[2];
+				return ;
+			}
+		}
+		else if (diff12 == 1)
+		{
+			*(*int_values + 3) = str_len5[1];
+			if (get_diff_charset(char_inputs[str_len5[2]], char_inputs[*(*int_values + 4)]) == 2)
+			{
+				*(*int_values + 5) = str_len5[2];
+				*(*int_values + 2) = str_len5[0];
+				return ;
+			}
+			else
+			{
+				*(*int_values + 5) = str_len5[0];
+				*(*int_values + 2) = str_len5[2];
+				return ;
+			}
+		}
+	}
+	//Means str_len5[0] and str_len5[1] are 2 and 5
+	//		str_len5[2] is 3
+	else 
+	{
+		*(*int_values + 3) = str_len5[2];
+		if (get_diff_charset(char_inputs[str_len5[0]], char_inputs[*(*int_values + 4)]) == 2)
+		{
+			*(*int_values + 5) = str_len5[0];
+			*(*int_values + 2) = str_len5[1];
+			return ;
+		}
+		else
+		{
+			*(*int_values + 5) = str_len5[1];
+			*(*int_values + 2) = str_len5[0];
+			return ;
+		}
+	}
+}
+
+void	decipher_inputs(char **char_inputs, int **int_values)
+{
+	int		i, i5, i6, word_len;
+	int		*str_len5, *str_len6;
+
+	i = 0;
+	i5 = 0;
+	i6 = 0;
+	word_len = 0;
+	str_len5 = malloc(3 * sizeof(int));
+	str_len6 = malloc(3 * sizeof(int));
+	while (i < 10)
+	{
+		word_len = strlen(char_inputs[i]);
+		if (word_len == 2)
+			*(*int_values + 1) = i;
+		else if (word_len == 3)
+			*(*int_values + 7) = i;
+		else if (word_len == 4)
+			*(*int_values + 4) = i;
+		else if (word_len == 5)
+			str_len5[i5++] = i;
+		else if (word_len == 6)
+			str_len6[i6++] = i;
+		else if (word_len == 7)
+			*(*int_values + 8) = i;
+		i++;
+	}
+	decipher_5s(char_inputs, int_values, str_len5);
+	decipher_6s(char_inputs, int_values, str_len6);
+}
+
+void	reverse_indices(int *int_values)
+{
+	int buffer[10];
+	int	i;
+
+	i = 0;
+	while (i < 10)
+	{
+		buffer[i] = int_values[i];
+		i++;
+	}
+	i = 0;
+	while (i < 10)
+	{
+		*(int_values + buffer[i]) = i;
+		i++;
+	}
+}
+
+int	get_abs_diff(char *s1, char *s2, int n)
+{
+	int	i;
+
+	i = 0;
+	if (n != strlen(s2))
+		return (1);
+	while (i < n)
+	{
+		if (!is_set(s1[i], s2))
+			return (1);
+		i++; 
+	}
+	return (0);
+}
+
+int	get_digit(char *curr_digit, int word_len, char **char_inputs)
+{
+	int	i;
+
+	i = 0;
+	while (get_abs_diff(curr_digit, char_inputs[i], word_len))
+		i++;
+	return (i);
+}
+
+int	solve_line(char **char_inputs, char *curr_line, int *int_values)
+{
+	int		i;
+	int		multiplier, result, curr_digit;
+	int		word_len;
+
+	i = 0;
+	result = 0;
+	multiplier = 1000;
+	//I now want to search from word index to digit value
+	reverse_indices(int_values);
+	while (curr_line[++i] != '|') ;
+	while (!isalpha(curr_line[++i])) ;
+	while (multiplier && curr_line[i])
+	{
+		word_len = 0;
+		while (isalpha(curr_line[i]))
+		{
+			i++;
+			word_len++;
+		}
+		curr_digit = get_digit(curr_line + i - word_len, word_len, char_inputs);
+		printf("for multiplier = %d, digit = %d\n", multiplier, curr_digit);
+		printf("int_values[digit] = %d\n", int_values[curr_digit]);
+		result += multiplier * int_values[curr_digit];
+		multiplier /= 10;
+		while (!isalpha(curr_line[i]))
+			i++;
+	}
+	return (result);
+}
+
 int	main(void)
 {
-	int		fd, i, word_len;
-	char	*file_buff;
-	int		*digits_count;
+	int		fd, i;
+	int		curr_number;
+	char	*curr_line;
+	char	**char_inputs;
+	int		*int_values;
 
-	word_len = 0;
-	digits_count = malloc(10 * sizeof(int));
-	for (i = 0; i < 10; i++)
-		digits_count[i] = 0;
 	fd = open("input.txt", O_RDONLY);
-	file_buff = get_next_line(fd);
-	while (file_buff)
-	{
-		count_1478(file_buff, &digits_count);
-		free(file_buff);
-		file_buff = get_next_line(fd);
-	}
+	curr_line = get_next_line(fd);
+	int_values = malloc(10 * sizeof(int));
+	for (i = 0; i < 10; i++)
+		int_values[i] = 0;
+	// while (curr_line)
+	// {
+		char_inputs = parse_line(curr_line);
+		decipher_inputs(char_inputs, &int_values);
+	printf("curr_line = %s", curr_line);
+	for (i = 0; i < 10; i++)
+		printf("int_values[%d] = %d\n", i, int_values[i]);
+	for (i = 0; i < 10; i++)
+		printf("int_values[%d] = %d\n", i, int_values[i]);
+
+	printf("Sol is %d\n", solve_line(char_inputs, curr_line, int_values));
+
+	for (i = 0; i < 10; i++)
+		printf("int_values[%d] = %d\n", i, int_values[i]);
+	// curr_number = solve_line(char_inputs, curr_line, int_values);
+	
+	free(curr_line);
+		// curr_line = get_next_line(fd);
+	// }
 	close(fd);
-	printf("# of 1s = %d\n", digits_count[1]);
-	printf("# of 4s = %d\n", digits_count[4]);
-	printf("# of 7s = %d\n", digits_count[7]);
-	printf("# of 8s = %d\n", digits_count[8]);
-	printf("sum = %d\n", digits_count[1] + digits_count[4] + digits_count [7] + digits_count[8]);
+	// for (i = 0; char_inputs[i]; i++)
+	// 	printf("line #%d: %s\n", i, char_inputs[i]);
+	// free_array(char_inputs);
+	// free(int_values);
 	return (0);
 }
 
